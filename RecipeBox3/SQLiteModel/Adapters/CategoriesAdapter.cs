c06da60552064fa9150fba868c38ee7609a77f22
Fox <billiamfo@gmail.com>
2018-01-29 15:58:30 -0500

    complete CategoriesAdapter

--- RecipeBox3/SQLiteModel/Adapters/CategoriesAdapter.cs
+++ RecipeBox3/SQLiteModel/Adapters/CategoriesAdapter.cs
@@ -1,14 +1,165 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RecipeBox3.SQLiteModel.Data;
 
 namespace RecipeBox3.SQLiteModel.Adapters
 {
-    class CategoriesAdapter : SQLiteAdapter
+    public sealed class CategoriesAdapter : SQLiteAdapter<Category>
     {
+        private SQLiteParameter idParameter;
+        private SQLiteParameter nameParameter;
 
+        /// <summary>
+        /// Create a new adapter with the application default connection string
+        /// </summary>
+        public CategoriesAdapter() : base() { }
+
+        /// <summary>
+        /// Create a new adapter with the specified connection string
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public CategoriesAdapter(string connectionString) : base(connectionString) { }
+
+        /// <summary>
+        /// Called from base constructor
+        /// </summary>
+        /// <param name="connectionString"></param>
+        protected override void Initialize(string connectionString)
+        {
+            base.Initialize(connectionString);
+
+            idParameter = new SQLiteParameter("@id", DbType.Int32);
+            nameParameter = new SQLiteParameter("@name", DbType.String);
+
+            SelectCommand.CommandText = "SELECT `C_ID`, `C_Name` FROM `Categories` WHERE (@id IS NULL) OR (`C_ID`=@id)";
+            SelectCommand.Parameters.Add(idParameter);
+
+            UpdateCommand.CommandText = "UPDATE `Categories` SET `C_Name`=@name WHERE `C_ID`=@id";
+            UpdateCommand.Parameters.Add(idParameter);
+            UpdateCommand.Parameters.Add(nameParameter);
+
+            InsertCommand.CommandText = "INSERT INTO `Categories` (`C_Name`) VALUES (@name)";
+            InsertCommand.Parameters.Add(nameParameter);
+
+            DeleteCommand.CommandText = "DELETE FROM `Categories` WHERE `C_ID`=@id";
+            DeleteCommand.Parameters.Add(idParameter);
+        }
+
+        /// <summary>Retrieve a list of <see cref="Category"/> objects from the Categories table</summary>
+        /// <returns>List containing data from database or null if there is no connection</returns>
+        public override List<Category> Select()
+        {
+            if (SelectCommand?.Connection == null) return null;
+            else
+            {
+                List<Category> results = new List<Category>();
+                idParameter.Value = null;
+
+                using (var reader = SelectCommand.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        Category nextRow;
+
+                        while (reader.Read())
+                        {
+                            try
+                            {
+                                nextRow = new Category(reader.GetInt32(0), reader.GetString(1));
+                                results.Add(nextRow);
+                            }
+                            catch (InvalidCastException) { }
+                        }
+                    }
+                }
+
+                return results;
+            }
+        }
+
+        /// <summary>
+        /// Get a specific <see cref="Category"/> by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public override Category Select(int id)
+        {
+            if (SelectCommand?.Connection == null) return null;
+            else
+            {
+                idParameter.Value = id;
+                Category row = null;
+
+                using (var reader = SelectCommand.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+
+                        try
+                        {
+                            row = new Category(reader.GetInt32(0), reader.GetString(1));
+                        }
+                        catch (InvalidCastException) { }
+                    }
+                }
+
+                return row;
+            }
+        }
+
+        /// <summary>
+        /// Update a <see cref="Category"/> in the database
+        /// </summary>
+        /// <param name="row"><see cref="Category"/> to update</param>
+        /// <returns>True if the row was updated successfully</returns>
+        public override bool Update(Category row)
+        {
+            idParameter.Value = row.C_ID;
+            nameParameter.Value = row.C_Name;
+            
+            return (UpdateCommand.ExecuteNonQuery() > 0);
+        }
+
+        /// <summary>
+        /// Insert a new <see cref="Category"/> into the database
+        /// </summary>
+        /// <param name="row"><see cref="Category"/> containing values to insert</param>
+        /// <returns>True if the row was inserted successfully</returns>
+        public override bool Insert(Category row)
+        {
+            nameParameter.Value = row.C_Name;
+
+            return (InsertCommand.ExecuteNonQuery() > 0);
+        }
+
+        /// <summary>
+        /// Delete a <see cref="Category"/> from the database
+        /// </summary>
+        /// <param name="row"><see cref="Category"/> to delete</param>
+        /// <returns>True if the row was deleted successfully</returns>
+        public override bool Delete(Category row)
+        {
+            idParameter.Value = row.C_ID;
+
+            return (DeleteCommand.ExecuteNonQuery() > 0);
+        }
+
+        /// <summary>
+        /// Delete a <see cref="Category"/> by id
+        /// </summary>
+        /// <param name="id">C_ID of <see cref="Category"/> to delete</param>
+        /// <returns>True if the row was deleted successfully</returns>
+        public override bool Delete(int id)
+        {
+            idParameter.Value = id;
+
+            return (DeleteCommand.ExecuteNonQuery() > 0);
+        }
     }
 }
--- RecipeBox3/SQLiteModel/Adapters/SQLiteAdapter.cs
+++ RecipeBox3/SQLiteModel/Adapters/SQLiteAdapter.cs
@@ -8,14 +8,25 @@ using RecipeBox3.SQLiteModel.Data;
 
 namespace RecipeBox3.SQLiteModel.Adapters
 {
-    public abstract class SQLiteAdapter
+    public abstract class SQLiteAdapter<T> where T : CookbookRow
     {
+        protected string _connectionString;
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+            set
+            {
+                _connectionString = value;
+                Connection.ConnectionString = value;
+            }
+        }
+
         protected SQLiteConnection _connection;
         protected SQLiteConnection Connection
         {
             get
             {
-                if (_connection == null) InitConnection();
+                if (_connection == null) Connection = new SQLiteConnection(ConnectionString);
                 return _connection;
             }
             set
@@ -24,36 +35,103 @@ namespace RecipeBox3.SQLiteModel.Adapters
                 else
                 {
                     _connection = value;
-                    if (DataAdapter?.SelectCommand != null) DataAdapter.SelectCommand.Connection = _connection;
-                    if (DataAdapter?.InsertCommand != null) DataAdapter.InsertCommand.Connection = _connection;
-                    if (DataAdapter?.UpdateCommand != null) DataAdapter.UpdateCommand.Connection = _connection;
-                    if (DataAdapter?.DeleteCommand != null) DataAdapter.DeleteCommand.Connection = _connection;
+                    if (SelectCommand != null) SelectCommand.Connection = _connection;
+                    if (InsertCommand != null) InsertCommand.Connection = _connection;
+                    if (UpdateCommand != null) UpdateCommand.Connection = _connection;
+                    if (DeleteCommand != null) DeleteCommand.Connection = _connection;
                 }
             }
         }
 
-        public SQLiteDataAdapter DataAdapter;
+        protected SQLiteCommand SelectCommand;
+        protected SQLiteCommand InsertCommand;
+        protected SQLiteCommand UpdateCommand;
+        protected SQLiteCommand DeleteCommand;
 
         public bool ClearBeforeFill { get; set; }
 
-        public SQLiteAdapter()
+        protected SQLiteAdapter() : this(Properties.Settings.Default.SQLiteConnectionString)
         {
-
+            
         }
 
-        protected void InitConnection()
+        protected SQLiteAdapter(string connectionString)
         {
-            Connection = new SQLiteConnection(Properties.Settings.Default.SQLiteConnectionString);
+            Initialize(connectionString);
         }
 
-        protected void InitAdapter()
+        protected virtual void Initialize(string connectionString)
         {
+            _connectionString = connectionString;
 
+            SelectCommand = new SQLiteCommand();
+            InsertCommand = new SQLiteCommand();
+            UpdateCommand = new SQLiteCommand();
+            DeleteCommand = new SQLiteCommand();
+
+            Connection = new SQLiteConnection(_connectionString);
         }
 
-        public List<T> GetData<T>() where T : CookbookRow
+        public abstract List<T> Select();
+
+        public abstract T Select(int id);
+
+        /// <summary>
+        /// Update a <see cref="CookbookRow"/>
+        /// </summary>
+        /// <param name="row">Row to update</param>
+        /// <returns>True if the row was updated, otherwise false</returns>
+        public abstract bool Update(T row);
+
+        /// <summary>
+        /// Update each <see cref="CookbookRow"/> in an enumerable set
+        /// </summary>
+        /// <param name="rows">set of rows to update</param>
+        public int Update(IEnumerable<T> rows)
         {
-            return null;
+            int rowsAffected = 0;
+
+            foreach (T row in rows)
+            {
+                if (Update(row)) rowsAffected += 1;
+            }
+
+            return rowsAffected;
+        }
+
+        /// <summary>
+        /// Insert a new <see cref="CookbookRow"/> into the database
+        /// </summary>
+        /// <param name="row">Row containing values to insert</param>
+        /// <returns>True if the row was successfully inserted</returns>
+        public abstract bool Insert(T row);
+
+        public int Insert(IEnumerable<T> rows)
+        {
+            int rowsInserted = 0;
+
+            foreach (T row in rows)
+            {
+                if (Insert(row)) rowsInserted += 1;
+            }
+
+            return rowsInserted;
+        }
+
+        public abstract bool Delete(int id);
+
+        public abstract bool Delete(T row);
+
+        public int Delete(IEnumerable<T> rows)
+        {
+            int rowsDeleted = 0;
+
+            foreach (T row in rows)
+            {
+                if (Delete(row)) rowsDeleted += 1;
+            }
+
+            return rowsDeleted;
         }
     }
 }
